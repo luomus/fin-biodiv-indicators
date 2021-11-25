011289a4a730318ec82bdb3dfda4d76fc3e962d0
wkmor1 <willi@mmorris.email>
2021-11-25 14:08:22 +0200

    Add port to cronjob

--- curl/job.sh
+++ curl/job.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-s=$(curl -v $HOST'/list/indices' | sed 's/,/ /g' | sed 's/"//g')
+s=$(curl -v $HOST:$PORT'/list/indices' | sed 's/,/ /g' | sed 's/"//g')
 
 for i in ${s:1:-1}
 
-  do curl -v $HOST'/ms-plot?cache=false&index='$i --output /dev/null
+  do curl -v $HOST:$PORT'/ms-plot?cache=false&index='$i --output /dev/null
 
 done
