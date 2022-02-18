bb5bf10dcffb3bb669d5c14e1a9676c09c434315
wkmor1 <willi@mmorris.email>
2022-02-18 13:06:20 +0200

    Reimplement rbms index calculation

--- pkg/R/run_model.R
+++ pkg/R/run_model.R
@@ -62,7 +62,6 @@ run_trim <- function(index, taxon, counts) {
 }
 
 #' @importFrom config get
-#' @importFrom rtrim count_summary
 
 run_rbms <- function(index, taxon, surveys, counts) {
 
@@ -74,28 +73,22 @@ run_rbms <- function(index, taxon, surveys, counts) {
 
   args[["byr"]] <- config::get("model", config = index)[["rbms"]][["base_year"]]
 
-  model_data <- do.call(rbms, args)
-
-  attr(model_data, "count_summary") <- rtrim::count_summary(
-    as.data.frame(counts), "abundance", "location_id"
-  )[-2L]
-
-  model_data
+  do.call(rbms, args)
 
 }
 
-#' @importFrom dplyr collect .data filter mutate select
+#' @importFrom dplyr arrange collect .data filter group_by lag lead mutate
+#' @importFrom dplyr select summarise
 #' @importFrom rbms boot_sample collated_index flight_curve impute_count
 #' @importFrom rbms site_index ts_dwmy_table ts_monit_count_site ts_monit_season
 #' @importFrom rbms ts_monit_site
-#' @importFrom stats quantile
+#' @importFrom rtrim count_summary
+#' @importFrom stats sd
 
 rbms <- function(
   counts,
   surveys,
   byr,
-  lower_ci = .159,
-  upper_ci = .841,
   StartMonth = 4,
   EndMonth = 9,
   StartDay = 1,
@@ -135,6 +128,8 @@ rbms <- function(
   init_year <- min(surveys[["year"]])
 
   last_year <- max(surveys[["year"]])
+
+  nyears <- last_year - init_year + 1L
 
   ts_date <- rbms::ts_dwmy_table(InitYear = init_year, LastYear = last_year)
 
@@ -195,44 +190,99 @@ rbms <- function(
     MultiVisit      = MultiVisit
   )
 
+  count_summary <- rtrim::count_summary(
+    impt_counts, "IMPUTED_COUNT", "SITE_ID", "YEAR"
+  )[-2L]
+
   sindex <- rbms::site_index(
     butterfly_count = impt_counts, MinFC = MinFC
   )
 
-  index_mean <- rbms::collated_index(data = sindex, s_sp = 1L)
-
-  index_mean <- index_mean[["col_index"]][["COL_INDEX"]]
-
-  ref <- index_mean[[base]]
-
-  index_mean_scaled <- index_mean / ref
-
   bootsample <- rbms::boot_sample(data = sindex, boot_n = boot_n)
 
-  index_b <- mapply(
+  index_mc <- mapply(
     rbms::collated_index,
     bootID = seq_len(boot_n),
     MoreArgs = list(data = sindex, s_sp = 1L, boot_ind = bootsample),
     SIMPLIFY = FALSE
   )
 
-  index_b <- lapply(index_b, getElement, "col_index")
+  index_mc <- lapply(index_mc, getElement, "col_index")
 
-  index_b <- do.call(rbind, index_b)
+  index_mc <- do.call(rbind, index_mc)
 
-  index_interval <- tapply(
-    index_b[["COL_INDEX"]] / ref, index_b[["M_YEAR"]], stats::quantile,
-    c(lower_ci, upper_ci)
+  index_mc <- dplyr::mutate(
+    index_mc, mc = log(.data[["COL_INDEX"]]), time = .data[["M_YEAR"]]
   )
 
-  index_interval <- do.call(rbind, index_interval)
+  index_mc <- dplyr::group_by(index_mc, .data[["BOOTi"]])
 
-  data.frame(
-    time  = seq.int(init_year, last_year),
-    mean  = index_mean_scaled,
-    sd    = (index_interval[, 2L] - index_interval[, 1L]) / 2,
-    lower = index_interval[, 1L],
-    upper = index_interval[, 2L]
+  index_mc <- dplyr::arrange(index_mc, .data[["time"]])
+
+  index_mc <- dplyr::mutate(
+    index_mc, mcf = dplyr::lead(.data[["mc"]]) - .data[["mc"]]
   )
+
+  index_mc <- dplyr::arrange(index_mc, -.data[["time"]])
+
+  index_mc <- dplyr::mutate(
+    index_mc, mcb = dplyr::lead(.data[["mc"]]) - .data[["mc"]]
+  )
+
+  index_mc <- dplyr::arrange(index_mc, .data[["time"]])
+
+  index_mc <- dplyr::mutate(
+    index_mc,
+    mcf = dplyr::lead(.data[["mcf"]], base - 1L)
+  )
+
+  index_mc <- dplyr::mutate(
+    index_mc,
+    mcf = dplyr::lag(.data[["mcf"]], base, 0)
+  )
+
+  index_mc <- dplyr::mutate(
+    index_mc,
+    mcf = cumsum(.data[["mcf"]])
+  )
+
+  index_mc <- dplyr::arrange(index_mc, -.data[["time"]])
+
+  index_mc <- dplyr::mutate(
+    index_mc,
+    mcb = dplyr::lead(.data[["mcb"]], nyears - base)
+  )
+
+  index_mc <- dplyr::mutate(
+    index_mc,
+    mcb = dplyr::lag(.data[["mcb"]], nyears - base + 1L, 0)
+  )
+
+  index_mc <- dplyr::mutate(
+    index_mc,
+    mcb = cumsum(.data[["mcb"]])
+  )
+
+  index_mc <- dplyr::group_by(index_mc, .data[["time"]])
+
+  index <- dplyr::summarise(
+    index_mc,
+    mean =
+      exp(mean(.data[["mcf"]])) *
+      exp(mean(.data[["mcb"]])),
+    sd =
+      stats::sd(.data[["mcf"]]) * exp(mean(.data[["mcf"]])) +
+      stats::sd(.data[["mcb"]]) * exp(mean(.data[["mcb"]]))
+  )
+
+  index <- dplyr::mutate(
+    index,
+    lower = .data[["mean"]] - .data[["sd"]],
+    upper = .data[["mean"]] + .data[["sd"]]
+  )
+
+  attr(index, "count_summary") <- count_summary
+
+  index
 
 }
