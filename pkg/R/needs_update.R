ec09e97da68bd6e415b168a51568ddf900c99494
wkmor1 <willi@mmorris.email>
2022-10-05 11:11:28 +0300

    Merge changes from 'dev'

--- pkg/R/needs_update.R
+++ pkg/R/needs_update.R
@@ -1,3 +1,5 @@
+#' @importFrom config get
+
 needs_update <- function(index, filter, table, db) {
 
   ans <- Sys.getenv("CHECK_UPDATE", TRUE)
@@ -14,7 +16,19 @@ needs_update <- function(index, filter, table, db) {
 
     last_cached_on <- last_cached(index, table, db)
 
-    last_mod_date > last_cached_on
+    out_of_date <- last_mod_date > last_cached_on
+
+    current_year <- as.integer(format(Sys.Date(), "%Y"))
+
+    use_after_date <- config::get("use_data_after", config = index)
+
+    use_after_date <- as.Date(paste(current_year, use_after_date, sep = "-"))
+
+    use_current_year <- Sys.Date() >= use_after_date
+
+    cached_before_using <- last_cached_on < use_after_date
+
+    out_of_date | use_current_year & cached_before_using
 
   }
 
