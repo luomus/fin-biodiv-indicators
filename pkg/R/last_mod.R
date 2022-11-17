f3626cbaf167df16a7666e4b03c7ac3fde295f12
wkmor1 <willi@mmorris.email>
2022-11-17 08:39:04 +0200

    Fix last mod output when there is no data

--- pkg/R/last_mod.R
+++ pkg/R/last_mod.R
@@ -2,11 +2,17 @@
 
 last_mod <- function(filter) {
 
-  ans <- finbif::finbif_occurrence(
+  res <- finbif::finbif_occurrence(
     filter = filter, select = "load_date", order_by = "-load_date", n = 1L,
     quiet = TRUE
   )
 
-  as.Date(ans[[1L, 1L]])
+  ans <- as.Date(NULL)
+
+  if (nrow(res) > 1L) {
+
+    ans <- as.Date(res[[1L, 1L]])
+
+  }
 
 }
