fb329fc7a7a5351e2dde0df5339e6b5a806c953e
wkmor1 <willi@mmorris.email>
2026-05-13 11:11:21 +0300

    Set use DwC fields default to FALSE

--- run_update.R
+++ run_update.R
@@ -17,7 +17,8 @@ options(
   finbif_email = Sys.getenv("FINBIF_EMAIL"),
   finbif_retry_times = 10,
   finbif_retry_pause_base = 2,
-  finbif_retry_pause_cap = 5e3
+  finbif_retry_pause_cap = 5e3,
+  finbif_use_dwc = FALSE
 )
 
 pool <- pool::dbPool(RPostgres::Postgres())
